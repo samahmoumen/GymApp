44ba2eeef8a1454a2dc1318c087299ea3560c7de
Lenovo <samahmn98@gmail.com>
2024-07-15 13:20:25 +0100

    new

--- development-platform-sa123-master/members/members/src/main/java/com/example/members/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.members.exception;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(String message){
-        super(message);
-    }
-}
